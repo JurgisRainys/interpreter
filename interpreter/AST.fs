d446f44938bad42ddce7b91bd85759fbc8705a6d
JurgisRainys <Jurgiuir@gmail.com>
2018-04-29 15:58:42 +0300

    initial

---
+++ interpreter/AST.fs
@@ -0,0 +1,19 @@
+﻿namespace interpreter
+
+type AST = 
+    | NewAssignment of (Vartype * string * string)
+    | Vart of Vartype
+
+type Vartype =
+    | INT
+    | STR
+    | BOOL   
+
+(*
+    <FsYacc Include="lexer-parser-rules/Parser.fsy">
+      <OtherFlags>--module Parser</OtherFlags>
+    </FsYacc>
+    <FsLex Include="lexer-parser-rules/Lexer.fsl">
+      <OtherFlags>--unicode</OtherFlags>
+    </FsLex>
+*)
---
+++ interpreter/Program.fs
@@ -0,0 +1,10 @@
+﻿namespace interpreter
+
+open System
+namespace interpreter
+ 
+module Entry = 
+    [<EntryPoint>]
+    printfn "Press any key to continue..."
+    System.Console.ReadLine() |> ignore
+    
